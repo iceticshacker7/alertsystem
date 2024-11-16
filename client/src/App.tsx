4405de0fce131af4a6484124329ada09eeadfd65
Suryansh2188 <suryamail4440@gmail.com>
2024-11-16 22:37:29 +0530

    !Logged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-import Component from "./component/Alert"
+import Component from "./component/Alert";
+import Auth from './pages/Auth'
+import { onAuthStateChanged } from "firebase/auth";
+import {auth} from './lib/firebase'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [])
+  
   return (
-    <><Component/></>
+  user ? <Component user={user}/> : <Auth />
   )
 }
 
